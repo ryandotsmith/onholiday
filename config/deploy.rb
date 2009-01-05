d292c573335fb728824a2eb55e25ef39a34a69ce
Ryan Smith <this.ryansmith@gmail.com>
2009-01-05 13:47:01 -0600

    modified the deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,15 +1,62 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+#############################################################
+#	Application
+#############################################################
 
-# If you aren't deploying to /u/apps/#{application} on the target
-# servers (which is the default), you can specify the actual location
-# via the :deploy_to variable:
-# set :deploy_to, "/var/www/#{application}"
+set :application, "onholiday"
+set :deploy_to, "/var/app/#{application}"
 
-# If you aren't using Subversion to manage your source code, specify
-# your SCM below:
-# set :scm, :subversion
+#############################################################
+#	Settings
+#############################################################
 
-role :app, "your app-server here"
-role :web, "your web-server here"
-role :db,  "your db-server here", :primary => true
+default_run_options[:pty] = true
+set :use_sudo, false
+
+#############################################################
+#	Servers
+#############################################################
+
+set :user, "rsmith"
+set :domain, "10.0.1.20"
+server domain, :app, :web
+role :db, domain, :primary => true
+
+#############################################################
+#	Subversion
+#############################################################
+
+#set :repository,  "svn://10.0.1.18/var/repository/apply"
+#set :svn_username, "rsmith"
+#set :svn_password, "rsmith"
+#set :checkout, "export"
+
+#############################################################
+#	Git
+#############################################################
+set :scm, :git
+set :repository, "git@github.com:ryandotsmith/onholiday.git"
+set :branch, "master"
+set :deploy_via, :remote_cache
+
+
+#############################################################
+#	Passenger
+#############################################################
+namespace :deploy do
+  
+  desc "Restart Application"
+  task :restart, :roles => :app do
+    run "touch #{current_path}/tmp/restart.txt"
+    run "mkdir #{current_path}/tmp/sessions"
+  end
+
+  desc "Symlink shared configs and folders on each release."
+    task :symlink_shared do
+      run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+    end
+      
+end
+after 'deploy:update_code', 'deploy:symlink_shared'
+#############################################################
+#	Attachment_FU 
+#############################################################
