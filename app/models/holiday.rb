ae0e95bd9a6fc22af519c139f22e061b1de0e29b
Ryan Smith <this.ryansmith@gmail.com>
2009-01-12 09:52:57 -0600

    changed the user index page

--- app/models/holiday.rb
+++ app/models/holiday.rb
@@ -6,7 +6,7 @@ class Holiday < ActiveRecord::Base
   # and should return
   #=>
   def self.get_holiday_types
-    ["etc","vacation","personal"]  
+    ["personal","etc","vacation"]  
   end
   ####################
   #get_pending should get
