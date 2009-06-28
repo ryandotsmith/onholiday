8afbfbfa380d394ccd287103e100073e61637d8b
Ryan  Smith <rsmith@cgull.local>
2009-06-27 19:16:28 -0500

    updated deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -57,8 +57,6 @@ namespace :deploy do
   desc "Symlink shared configs and folders on each release."
     task :symlink_shared do
       run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
-      run "touch #{release_path}/config/environment.rb"
-      run "ln -nfs #{shared_path}/config/environment.rb #{release_path}/config/environment.rb"
       run "chmod 755 #{release_path}/script/delayed_job"
     end
       
