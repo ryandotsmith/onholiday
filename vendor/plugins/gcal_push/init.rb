152db7e5c87a3efec8031dfc49e79c9a21bc5004
ryan r. smith <rsmith@teeton.local>
2010-01-30 10:13:23 -0600

    removed the gcal_push plugin

--- vendor/plugins/gcal_push/init.rb
+++
@@ -1,9 +0,0 @@
-$:.unshift(File.dirname(__FILE__)) unless
-$:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
-require 'gdata'
-require 'hpricot'
-require 'yaml'
-require 'ostruct'
-
-require File.dirname(__FILE__) + '/lib/gcal'
-ActiveRecord::Base.send(:include, Gcal )
